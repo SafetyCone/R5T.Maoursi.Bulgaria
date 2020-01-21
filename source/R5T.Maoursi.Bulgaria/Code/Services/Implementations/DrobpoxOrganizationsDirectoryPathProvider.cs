dd19b9be98774b3379ad878f12808790020fd420
MinexAutomation <minex.automation@gmail.com>
2020-01-21 10:41:08 -0800

    Added DropboxOrganizationsDirectoryPathProvider implementation of the IOrganizationsDirectoryPathProvider service.

---
+++ source/R5T.Maoursi.Bulgaria/Code/Services/Implementations/DrobpoxOrganizationsDirectoryPathProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Bulgaria;
+using R5T.Kalamaria;
+using R5T.Lombardy;
+
+
+namespace R5T.Maoursi.Bulgaria
+{
+    public class DrobpoxOrganizationsDirectoryPathProvider : IOrganizationsDirectoryPathProvider
+    {
+        private IDropboxDirectoryPathProvider DropboxDirectoryPathProvider { get; }
+        private IOrganizationsDirectoryNameConvention OrganizationsDirectoryNameConvention { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public DrobpoxOrganizationsDirectoryPathProvider(
+            IDropboxDirectoryPathProvider dropboxDirectoryPathProvider,
+            IOrganizationsDirectoryNameConvention organizationsDirectoryNameConvention,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.DropboxDirectoryPathProvider = dropboxDirectoryPathProvider;
+            this.OrganizationsDirectoryNameConvention = organizationsDirectoryNameConvention;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetOrganizationsDirectoryPath()
+        {
+            var dropboxDirectoryPath = this.DropboxDirectoryPathProvider.GetDropboxDirectoryPath();
+
+            var organizationsDirectoryName = this.OrganizationsDirectoryNameConvention.GetOrganizationsDirectoryName();
+
+            var organizationsDirectoryPath = this.StringlyTypedPathOperator.GetDirectoryPath(dropboxDirectoryPath, organizationsDirectoryName);
+            return organizationsDirectoryName;
+        }
+    }
+}
