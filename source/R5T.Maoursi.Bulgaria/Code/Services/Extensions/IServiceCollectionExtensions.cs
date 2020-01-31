f3c32f3a5655702173e6ede788b94fe389a32b3b
MinexAutomation <minex.automation@gmail.com>
2020-01-30 16:08:20 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Maoursi.Bulgaria/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bulgaria;
+using R5T.Dacia;
+using R5T.Kalamaria;
+using R5T.Lombardy;
+
+
+namespace R5T.Maoursi.Bulgaria
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add the <see cref="DropboxOrganizationsDirectoryPathProvider"/> implementation for <see cref="IOrganizationsDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDropboxOrganizationsDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<IDropboxDirectoryPathProvider> addDropboxDirectoryPathProvider,
+            ServiceAction<IOrganizationsDirectoryNameConvention> addOrganizationsDirectoryNameConvention,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<IOrganizationsDirectoryPathProvider, DropboxOrganizationsDirectoryPathProvider>()
+                .RunServiceAction(addDropboxDirectoryPathProvider)
+                .RunServiceAction(addOrganizationsDirectoryNameConvention)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add the <see cref="DropboxOrganizationsDirectoryPathProvider"/> implementation for <see cref="IOrganizationsDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IOrganizationsDirectoryPathProvider> AddDropboxOrganizationsDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<IDropboxDirectoryPathProvider> addDropboxDirectoryPathProvider,
+            ServiceAction<IOrganizationsDirectoryNameConvention> addOrganizationsDirectoryNameConvention,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<IOrganizationsDirectoryPathProvider>(() => services.AddDropboxOrganizationsDirectoryPathProvider(
+                addDropboxDirectoryPathProvider,
+                addOrganizationsDirectoryNameConvention,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
